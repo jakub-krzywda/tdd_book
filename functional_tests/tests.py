0d24d517e9e468d83f3056b9078b12a120b1789d
Jakub Krzywda <qba0311@gmail.com>
2022-11-15 15:26:52 +0100

    Added new functional test in order to check if multiple users to-do lists are supported

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+
 import time
 from django.test import LiveServerTestCase
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -12,12 +16,21 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, item_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(item_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+    def test_can_start_a_list_for_one_user(self):
         # Mathew heard about new to-do list site
         # He goes to check its homepage
         self.browser.get(self.live_server_url)
@@ -37,7 +50,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         # "1: Raise taxes" as an item in to-do list
-        self.check_for_row_in_list_table('1: Raise taxes')
+        self.wait_for_row_in_list_table('1: Raise taxes')
         # There is still a text box inviting to enter another to-do list item. He enters
         # "Lie about raising taxes". He hits enter and now page lists both items.
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -45,11 +58,51 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Lie about raising taxes')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        self.check_for_row_in_list_table('1: Raise taxes')
-        self.check_for_row_in_list_table('2: Lie about raising taxes')
+        self.wait_for_row_in_list_table('1: Raise taxes')
+        self.wait_for_row_in_list_table('2: Lie about raising taxes')
 
-        self.fail('Finish the test!')
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Mateusz starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Raise taxes')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Raise taxes')
+        pinoccios_list_url = self.browser.current_url
+        self.assertRegex(pinoccios_list_url, '/lists/.+')
         # Mateusz wonders if site will remember his to-do list. He notices that site generated
         # unique url for him. There is some explanatory text to that effect
 
         # He visits that url - his to-do list is still there
+
+        # Now a new user, Jaroslaw, comes along to the site.
+
+        ## We use a new browser session to make sure that no information
+        ## of Mateusz's is coming through from cookies etc
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Jaroslaw visits the home page. There is no sign of Mateusz's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Raise taxes', page_text)
+        self.assertNotIn('Lie about raising taxes', page_text)
+
+        # Jaroslaw starts a new list by entering a new item. He is less interesting than Mateusz
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Fuck around')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Fuck around')
+
+        # Jaroslaw gets his own unique URL
+        jaroslaw_list_url = self.browser.current_url
+        self.assertRegex(jaroslaw_list_url, '/lists/.+')
+        self.assertNotEqual(pinoccios_list_url, jaroslaw_list_url)
+
+        # Again there is no sign of Mateusz's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Raise taxes', page_text)
+        self.assertIn('Fuck around', page_text)
+
+        # Satisfied, they both go back to sleep
